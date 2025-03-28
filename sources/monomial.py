38191cc16dfcc7b285416f6b223ee31418001348
Geoffrey Dornic <gdornic.pro@gmail.com>
2025-03-28 19:06:30 +0100

    getReducedForm and getDegree functionnal

--- sources/monomial.py
+++ sources/monomial.py
@@ -1,12 +1,23 @@
 class Monomial:
-    def __init__(self, term):
+    def __init__(self, term: str):
         if '*' in term:
             self.coefficient = float(term.split('*')[0])
-        elif 'X' in term:
-            self.coefficient = 1
         else:
-            self.coefficient = float(term)
+            self.coefficient = 1
         if '^' in term:
             self.degree = int(term.split('^')[-1])
         else:
             self.degree = 0
+
+    def degree_sort_key(monomial):
+        return monomial.degree
+
+    def __add__(self, o):
+        if self.degree == o.degree:
+            return Monomial(f"{self.coefficient + o.coefficient}*X^{self.degree}")
+        return Monomial("0*X^0")
+
+    def __str__(self):
+        if self.coefficient < 0:
+            return f"- {abs(self.coefficient)} * X^{self.degree}"
+        return f"+ {self.coefficient} * X^{self.degree}"
--- sources/polynomial.py
+++ sources/polynomial.py
@@ -12,5 +12,33 @@ class Polynomial:
         for m in self.monomials:
             print("Coefficient: ", m.coefficient, ", Degree: ", m.degree)
 
+        self.reduce()
+
+        print()
+        for m in self.monomials:
+            print("Coefficient: ", m.coefficient, ", Degree: ", m.degree)
+
+        print(f"Reduced form: {self.getReducedForm()}")
+        print(f"Polynomial degree: {self.getDegree()}")
+    
+    def reduce(self):
+        self.monomials.sort(key=Monomial.degree_sort_key)
+        i = 0
+        while i < len(self.monomials) - 1:
+            if self.monomials[i].degree == self.monomials[i+1].degree:
+                monomials_sum = self.monomials[i] + self.monomials[i+1]
+                self.monomials[i] = monomials_sum
+                del self.monomials[i+1]
+            i+=1
+
+    def getReducedForm(self):
+        reduced_form = f"{" ".join(map(str, self.monomials))} = 0"
+        if reduced_form[0] == '-':
+            return reduced_form[0] + reduced_form[2:]
+        return reduced_form[2:]
+
+    def getDegree(self):
+        return max(self.monomials, key=Monomial.degree_sort_key).degree
+
     def printAnswer(self):
         pass
